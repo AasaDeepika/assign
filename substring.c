20544dfe4789673cf8f5abf8020790099e3f6ce4
AasaDeepika <ashadeepikaeee@gmail.com>
2016-02-07 14:24:45 +0530

    Create substring.c
    Write a program to find the  longest sub-string and length without repeating characters in a given string. 
    
    Ex:   s = “abcabc”, the longest sub-string without repeating characters is “abc” and it's the length is 3.

---
+++ substring.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main(void) 
+{
+	char str1[20],str2[20];
+	int i,j,count=1,flag=0;
+	scanf("%s",str1);
+	str2[0]=str1[0];
+	for(i=1;str1[i]!='\0';i++)
+	{
+		for(j=0;j<count;j++)
+		{
+			if(str1[i]==str2[j])
+			{
+				
+				flag=1;
+				break;
+			}
+		}
+		
+		if(flag==1)
+			break;
+		str2[i]=str1[i];
+		++count;
+	}
+	
+	for(i=0;i<count;i++)
+	{
+		printf("%c",str2[i]);
+	}
+	return 0;
+}
