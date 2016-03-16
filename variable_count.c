694e41f3ccb3259a4cc807fe2c35335571c26ac0
AasaDeepika <ashadeepikaeee@gmail.com>
2016-03-16 17:35:54 +0530

    Update variable_count.c

--- variable_count.c
+++ variable_count.c
@@ -1,44 +1,80 @@
 #include<stdio.h>
-#include<ctype.h>
 #include<string.h>
-int Check(char b[],int n);
+int isnum(char a[])
+{
+	
+	if((a[0]>=65 && a[0]<=90)||(a[0]>=97 && a[0]<=122))
+	{
+		return 0;
+	}
+	return 1;
+}
+
 int main()
 {
-	int l,i,j=0,count;
-	char a[20],b[20];
-	printf("\nEnter The Equation:");
+	int i,l,j=0,k=0;
+	char a[30],b[30],op[10][10];
+	printf("Enter the equation:");
 	scanf("%s",a);
 	l=strlen(a);
+	
 	for(i=0;i<l;i++)
 	{
-		if(isalpha(a[i]))
+		if((a[i]>=48 && a[i]<=57)||(a[i]>=97 && a[i]<=122)||(a[i]>=65 && a[i]<=90))
 		{
-			b[j]=a[i];
-			j++;
+			b[i]=a[i];
+		}
+		else 
+		{
+			b[i]='_';
 		}
 	}
-	count=Check(b,j);	
-	printf("\ncount:%d",count);
-	return 0;
-}
-int Check(char b[],int n)
-{
-	int i,j,k,count=0;
-	for(i=0;i<n;i++)		
+	
+	b[l]='_';
+	
+	for(i=0;i<l;i++)
 	{
-		for(j=i+1;j<n;)
+		if(b[i]=='_')
 		{
-			if(b[i] == b[j])
+			continue;
+		}
+		else
+		{
+			op[j][k]=b[i];
+			k++;
+			if(b[i+1]=='_')
 			{
-				for(k=j;k<n;k++)
-				{
-					b[k]=b[k+1];
-				}
-				n--;
-			}
-			else
+				op[j][k]='\0';
 				j++;
+				k=0;
+				i++;
+			}
 		}
 	}
-	return n;
+	
+	for(i=0;i<j;i++)
+	{
+		if(isnum(op[i]))
+		{
+			strcpy(op[i],"\0");
+		}
+	}
+	
+	for(i=0;i<j;i++)
+	{
+		if(strcmp(op[i],"\0")!=0)
+		{
+			for(k=i+1;k<j;k++)
+			{
+				if(strcmp(op[i],op[k])==0)
+				{
+				
+					strcpy(op[k],"\0");
+			
+				}
+			}
+			printf("%s ",op[i]);
+		}
+	} 
+	return 0;
 }
